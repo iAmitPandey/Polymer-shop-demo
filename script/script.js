707dbf51aa6c2a32b5e68f6e21de849c55d91a9f
Amit Pandey <amitpandey21599@gmail.com>
2024-10-13 22:37:56 +0530

    Add the polymer-shop-demo project

---
+++ script/script.js
@@ -0,0 +1,19 @@
+// function showSidebar(flag) {
+//   const sidebar = document.querySelector(".hidden");
+//   if (flag) {
+//     clickOnSideBar.style.visibility = "hidden";
+//     sidebar.style.display = "flex";
+//     sidebar.style.flexDirection = "column";
+//     sidebar.style.alignItems = "center";
+//   } else {
+//     sidebar.style.display = "none";
+//   }
+// }
+// let flag = false;
+// const clickOnSideBar = document.querySelector(".menu");
+// clickOnSideBar.onclick = () => {
+//   flag = !flag;
+
+//   showSidebar(flag);
+// };
+// showSidebar(flag);
